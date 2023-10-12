51e28c63f691300009938933cfd2fc1ce11aac85
carlos <carlosa@xgrow.com>
2023-10-12 04:37:38 -0300

    Feat: Create project

---
+++ src/test/java/br/com/rocketseat/ToDo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.ToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
